5252f53a88fdf43bda232bd7f573d141ff55f72f
Andreas Chaitidis <andreas.chaitidis@gmail.com>
2019-01-24 23:15:44 +0100

    correction TestSensorValue.ino

--- Examples/TestSensorValue/TestSensorValue.ino
+++ Examples/TestSensorValue/TestSensorValue.ino
@@ -5,14 +5,17 @@
 */
 
 #include "SEnsorValue.h"
+#include"HottModule.h"
 #define DEBUG
 
 // the setup function runs once when you press reset or power the board
 SensorValue<int>* sensor;
+GamModule * gam;
 
 void setup() {
 	Serial.begin(115200);
-	sensor = new SensorValue<int>("Sens1");
+	sensor = new SensorValue<int>("Sens1", gam);
+	gam = new GamModule();
 	Serial.print("getMinAlarm: "); Serial.println(sensor->minAlarm->getName());
 	Serial.print("getMaxAlarm: "); Serial.println(sensor->maxAlarm->getName());
 
