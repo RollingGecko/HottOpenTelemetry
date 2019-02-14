71a00c069182c8697b350efe08a82d5b580d8be5
Andreas Chaitidis <andreas.chaitidis@gmail.com>
2019-02-14 23:12:32 +0100

    Merged Test for sensor and value classes to one test file

--- test/Test_Sensor_Classes/Test_Sensor_Classes.cpp
+++ test/Test_Sensor_Classes/Test_Sensor_Classes.cpp
@@ -1,11 +1,48 @@
 
 #ifdef UNIT_TEST
+#include "Value.h"
 #include "SensorAlarm.h"
 #include "Mock_HottModule.h"
 #include <gtest/gtest.h>
 #include <stdint.h>
 
 #define GTEST
+
+// #define GTEST
+
+//Testing Class Value
+
+//Fixture for testing class Value
+class ValueClassTest :public testing::Test {
+protected:
+	
+	const char* valueName = "TestValue";
+	Value<uint8_t>* val;
+
+	ValueClassTest() {
+		
+		val = new Value<uint8_t>(valueName);
+		//Value<uint8_t> value(valueName);
+	}
+	~ValueClassTest() {
+	
+	}
+};
+TEST(ValueClass, ConstructorByName) {
+	const char name[] = "ValueClassName";
+	Value<uint8_t> val(name);
+
+	EXPECT_STREQ(name, val.getName());
+}
+
+TEST_F(ValueClassTest, getName) {
+	EXPECT_STREQ(valueName, val->getName());
+}
+
+TEST_F(ValueClassTest, set_uint8Value) {
+	val->setValue(100);
+	EXPECT_EQ(100, val->getValue());
+}
 
 //Testing Class SensorAlarm
 
